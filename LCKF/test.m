7eb642e106c757747a05283adb739b524aebc2a2
Lucas Alric <lucas.alric@polymtl.ca>
2024-03-11 16:31:48 -0400

    component update

--- LCKF/test.m
+++ LCKF/test.m
@@ -1,23 +1,9 @@
 dt = 1.5;
 A = [1, dt, (dt^2)/2; 0, 1, dt; 0, 0, 1];
+c = [1, 0, 0];
 constrain_vector = [0, 1, 0];
 
-c = [1, 0, 0];
-[row_count, col_count] = size(c);
-c_ = [];
+A = constr_component_a(A,constrain_vector,c);
 
-for i = 1:col_count
-    if c(i) == 1
-        % Créer une ligne de zéros
-        row = zeros(1, col_count);
-        % Mettre un 1 à la position correspondante
-        row(i) = 1;
-        % Ajouter cette ligne à la matrice de sortie
-        c_ = [c_; row];
-    end
-end
-
-c_*A*constrain_vector'
-
-
+disp(A)
 
